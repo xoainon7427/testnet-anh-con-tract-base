578c34c48fd9ade43bbaa28974f432eb9c0fcc68
xoainon7427 <xoaiconbasy@gmail.com>
2026-03-12 20:24:18 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,19 @@ services:
     image: nginx:latest
     ports:
       - "80:80"
+# Khai báo phiên bản định dạng của file Docker Compose (phiên bản 3)
+version: '3'
+
+# Định nghĩa danh sách các dịch vụ (containers) sẽ chạy trong hệ thống
+services:
+  # Tên của dịch vụ, ở đây đặt tên là 'web'
+  web:
+    # Chỉ định Image (hình ảnh mẫu) để khởi tạo container
+    # nginx:latest nghĩa là sử dụng phần mềm máy chủ web Nginx phiên bản mới nhất
+    image: nginx:latest
+
+    # Cấu hình ánh xạ cổng (port mapping)
+    ports:
+      # "Cổng_Máy_Host : Cổng_Container"
+      # Người dùng truy cập vào cổng 80 của máy tính sẽ được dẫn vào cổng 80 bên trong container
+      - "80:80"
