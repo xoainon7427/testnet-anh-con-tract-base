efab3b1b71c8afb5b768c4f95913b4509d1df8e2
xoainon7427 <xoaiconbasy@gmail.com>
2026-03-12 20:32:05 +0700

    Update db_connect.py

--- db_connect.py
+++ db_connect.py
@@ -4,8 +4,4 @@ end
 say_hi
 # Định nghĩa (khai báo) một phương thức (hàm) có tên là say_hi
 def say_hi
-  # Lệnh puts (put string) dùng để in nội dung ra màn hình và tự động xuống dòng
-  puts "Hello from Ruby script!"
-end
-
 
